6fc95168397faf647844831757f1691ea9d1da38
Ramesh Parthasarathy <msg.rameshp@gmail.com>
2017-06-24 20:05:44 -0400

    update

---
+++ proj-1/proj-1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  proj-1
+//
+//  Created by Ramesh Parthasarathy on 6/24/17.
+//  Copyright © 2017 Ramesh Parthasarathy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Thanks Dude!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
